be8b6190e4bfd72c154f3bd1aff0657e251cd636
Kostas Antonopoulos <kostas@devpro.gr>
2021-11-15 20:27:40 +0200

    [ADD] Minimum version for macOS and iOS in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
   name: "QRCodeReader",
+  platforms: [
+    .iOS(SupportedPlatform.IOSVersion.v12),
+    .macOS(SupportedPlatform.MacOSVersion.v10_14)
+  ],
   products: [
     .library(name: "QRCodeReader", targets: ["QRCodeReader"]),
   ],
